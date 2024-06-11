421e3bfb3e8f17028d31640c4af7cfc9a041e32e
yanakhazanova <yanakhazanova@gmail.com>
2024-06-11 17:14:33 +0300

    Fixed DataProcessor

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,23 +1,29 @@
-import os
 import pandas as pd
-import pandas_market_calendars as mcal
 
 class DataProcessor:
-    def __init__(self, ):
-        pass
-
+    def __init__(self, trading_days, start_date=None, end_date=None):
+        if trading_days is not None:
+            self.trading_days = trading_days
+            if start_date is not None and end_date is not None:
+                self.start_date = start_date
+                self.end_date = end_date
+            else:
+                self.start_date = min(trading_days['date'])
+                self.end_date = max(trading_days['date'])
+                
     def load_data(self, file_paths):
         data_frames = {}
         for key, path in file_paths.items():
             df = pd.read_csv(path, parse_dates=['date'])
-            data_frames[key] = pd.merge(self.trading_days, df, on='date', how='left')
-            data_frames[key].fillna(method='ffill', inplace=True)
-            data_frames[key].interpolate(method='linear', inplace=True)
+            merged_df = pd.merge(self.trading_days, df, on='date', how='left')
+            merged_df.ffill(inplace=True)
+            merged_df.interpolate(method='linear', inplace=True)
+            data_frames[key] = merged_df
         return data_frames
 
     def merge_data(self, data_frames):
         all_data = self.trading_days
         for key, df in data_frames.items():
             all_data = pd.merge(all_data, df, on='date', how='outer')
-        all_data.fillna(method='ffill', inplace=True)
+        all_data.ffill(inplace=True)
         return all_data
